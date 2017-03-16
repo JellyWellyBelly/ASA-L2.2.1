d971ecdcf18161ebae7d0f75ee46fe559eccc03a
Miguel Viegas <miegas4@users.noreply.github.com>
2017-03-16 21:28:27 +0000

    Add files via upload

--- main.c
+++ main.c
@@ -1,13 +1,23 @@
-#include <stdio.h>
-
+#include "mymacros.h"
 
 int main() {
 	
-	int nr_vertices, nr_input_lines;
+	int nr_vertices;
+	int nr_input_lines;
+	int vertex1;
+	int vertex2;
+	int i = 0;
 
-	scanf("%d %d", &nr_vertices, &nr_input_lines);
+	scanf("%d %d", &nr_vertices, &nr_input_lines); 		//le a primeira linha do stdin
 
-	printf("%d and %d\n", nr_vertices, nr_input_lines);
+	//Node *graph = malloc(sizeof(Node) * nr_vertices);	//Grafo representado num vetor de listas de adjancencias
+	
+	while(i < nr_input_lines) {
+		scanf("%d %d", &vertex1, &vertex2);
+		insert_list(graph[vertex1], vertex2);
+
+		i++;
+	}
 
 	return 0;
 }
